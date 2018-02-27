623cc3413b47a26316085c49342d60a7e82f8104
naty2004 <naty2004@naver.com>
2018-02-27 14:11:07 +0900

    solved exercise for session 1

--- study/임선우/ex.py
+++ study/임선우/ex.py
@@ -1,1 +1,15 @@
-'well....'
+a = input ("what is a?")
+if (a == '0'):
+    print ("a is zero")
+else:
+    print ("a is not zero")
+
+name = "seonwoo"
+character = ["cute","pretty","smart"]
+
+for i in character :
+    print (name, 'is', i)
+
+for i in range(1,4):
+    for j in range(1,4):
+        print (10*i + j)
