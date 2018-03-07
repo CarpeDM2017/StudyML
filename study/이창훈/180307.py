c35866286ca7e41387ea13577a27a976659bdbf6
hs10212 <37143712+hs10212@users.noreply.github.com>
2018-03-07 23:53:04 +0900

    최종 과제1

--- study/이창훈/180307.py
+++ study/이창훈/180307.py
@@ -4,8 +4,8 @@ except: print('Invalid input!')
 #여기서부터 작성해주시면 됩니다.
 
 for i in range(1, num+1):
-    spy=num+1-i
-    for j in range(spy-1):
+    spy=num-i
+    for j in range(spy):
         print(" ", end="")
     for k in range(i):
         print("*", end="")
