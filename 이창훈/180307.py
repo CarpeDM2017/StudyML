a7bd5b80c4ad997737ae16834baa27f5b8d8f531
hs10212 <37143712+hs10212@users.noreply.github.com>
2018-03-07 22:48:20 +0900

    Add 이창훈 folder
    과제중... 여백처리 미완

---
+++ 이창훈/180307.py
@@ -0,0 +1,10 @@
+num= input('Number of steps : ')
+try: num = int(num)
+except: print('Invalid input!')
+#여기서부터 작성해주시면 됩니다.
+
+for i in range(1, num+1):
+    spy=num+1-i
+    for j in range(spy):
+        print("*", end="")       
+    print()
